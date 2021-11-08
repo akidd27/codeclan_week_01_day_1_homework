27cdad1a56283c387d8443f0717f8025bd4bbce8
Adam Kidd <adamkidd3@gmail.com>
2021-11-08 17:01:31 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+#simple program containing variables of various types; methods; operators
+
+my_name = "Adam"
+my_age = 23
+earth_orbit_days = 365.25
+
+my_name_upper_case = my_name.upper()
+my_age_days = my_age*earth_orbit_days
+
+print(my_name_upper_case)
+print(my_age_days)
+print(type(my_age_days))
